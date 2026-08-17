7963fc900f29eb4c2db37077da7e490159e48e3f
Eduardo Jimenez Todd <eduardojimeneztodd@gmail.com>
2026-08-17 01:42:59 -0600

    Add umbrella public header

---
+++ include/signal_processing/signal_processing.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "signal_processing/convolution.hpp"
+#include "signal_processing/correlation.hpp"
+#include "signal_processing/dct.hpp"
+#include "signal_processing/fft.hpp"
+#include "signal_processing/fir.hpp"
+#include "signal_processing/iir.hpp"
+#include "signal_processing/windows.hpp"
